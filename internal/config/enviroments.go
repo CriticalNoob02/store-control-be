d1b08b666e6f551306fe7f817347f066fde57d5d
Adriel Rodrigues <adrieldchavea20@gmail.com>
2024-10-06 14:52:25 -0300

    feat: change database structure

--- internal/config/enviroments.go
+++ internal/config/enviroments.go
@@ -9,10 +9,10 @@ func GetLogLevel() string {
 	return os.Getenv("LOG_LEVEL")
 }
 
-func GetDbDsn() string {
+func GetDbUri() string {
 	return fmt.Sprintf(
 		"mongodb://%s:%s@%s:%s",
-		os.Getenv("DB_USER"),
+		os.Getenv("DB_USERNAME"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
