4b6e35437d2187507b881e241de1a1f3ac440e0a
Adriel Rodrigues <adrieldchavea20@gmail.com>
2024-10-06 14:51:33 -0300

    feat: add libs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/CriticalNoob02/store-control-be/internal/route"
+	"github.com/CriticalNoob02/store-control-be/pkg/util"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
+func main() {
+	err := godotenv.Load("./infra/env/.env")
+	util.Check(err)
+
+	api := gin.Default()
+
+	route.ClientRoutes(api)
+
+	api.Run()
 }
